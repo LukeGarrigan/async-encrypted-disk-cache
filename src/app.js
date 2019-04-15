2ff8290ae9f583706c29933b8c985e8408c242ce
Luke Garrigan <lukegarrigan8@gmail.com>
2019-04-15 06:13:14 +0100

    Updated everything according to .gitignore

--- src/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var FileSystemCache_1 = require("./FileSystemCache");
-var cache = new FileSystemCache_1.FileSystemCache("my-cache-folder", "secretKey");
-cache.set("key", "value").then(function (hasSet) {
-    console.log(hasSet);
-});
-//# sourceMappingURL=app.js.map
