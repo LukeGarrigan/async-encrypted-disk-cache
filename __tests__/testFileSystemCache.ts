f56bf6e6666e63517b758088b6f4fbd046668cf6
Luke Garrigan <lukegarrigan8@gmail.com>
2019-04-19 17:57:06 +0100

    Some changes for publishing

--- __tests__/testFileSystemCache.ts
+++ __tests__/testFileSystemCache.ts
@@ -1,4 +1,4 @@
-import {FileSystemCache} from "../src/FileSystemCache";
+import {FileSystemCache} from "../src/index";
 import {EncryptedCache} from "../src/EncryptedCache";
 import * as fs from "fs";
 let  os = require("os");
